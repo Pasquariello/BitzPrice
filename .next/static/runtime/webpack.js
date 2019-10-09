a3648c7bd571a3dd52b6d86fca9ae8dddeec7f46
Taylor <taylor@pasq.net>
2019-10-09 01:20:31 -0600

    add git ignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4952a56cd174b3e88549";
+/******/ 	var hotCurrentHash = "8b645db2ea6817f409c6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
